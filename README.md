7f07fbcceee482e1139bf8c14501e97eed771df5
Tim Van Steenburgh <tvansteenburgh@gmail.com>
2021-03-09 16:27:47 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# Greenhouse automation stuff
+
+## Install
+
+```bash
+git clone git@github.com:tvansteenburgh/greenhouse.git
+cd greenhouse
+virtualenv -p python3 .venv
+source .venv/bin/activate
+pip install selenium
+```
+
+## Duplicate job posts to different locations
+
+Start with a Greenhouse job with one job posting (the one that will be
+duplicated.
+
+Get the numeric id for the job. You can get this by going to the job
+dashboard and copying the id out of the url. It will look something like
+this:
+
+`https://canonical.greenhouse.io/sdash/1592880`
+
+That number at the end of the url is the job id.
+
+Now run the script and tell it which job id(s) you want to update with
+new job posts, and which regions you want to post in:
+
+```bash
+SSO_EMAIL=tvansteenburgh@gmail.com \
+SSO_PASSWORD=mysecretpassword \
+./job_post.py 1592880 1726996 --region americas emea
+```
+
+The browser will open and things will happen:
+
+- New posts will be created for each city in the region that doesn't
+  already have a post
+- The new posts (and any others that are OFF) will be turned ON (made
+  live on the 'Canonical - Jobs' board
+
+If the script fails partway through you can safely rerun it, since it won't
+create a duplicate job post for cities that already have one.
+
+### Browsers
+
+Default browser is Chrome but you can alternatively pass the `--browser firefox` option.
+
+I have only tested on Chrome.
+
+### Regions
+
+The available regions are `americas`, `emea`, and `apac`. You can
+view/update the lists of cities in those regions directly in the source
+file.
+
+
---
+++ post-job.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python3
+
+import argparse
+import contextlib
+import os
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import selenium.webdriver.support.ui as ui
+
+
+SSO_EMAIL = os.getenv('SSO_EMAIL')
+SSO_PASSWORD = os.getenv('SSO_PASSWORD')
+
+SITE = 'https://canonical.greenhouse.io'
+JOB_BOARD = 'Canonical - Jobs'
+
+REGIONS = {
+    'americas': [
+        # United States
+        'Home based - Americas, Atlanta',
+        'Home based - Americas, Austin',
+        'Home based - Americas, Boston',
+        'Home based - Americas, Chicago',
+        'Home based - Americas, Colorado',
+        'Home based - Americas, Dallas',
+        'Home based - Americas, Los Angeles',
+        'Home based - Americas, New York',
+        'Home based - Americas, Raleigh',
+        'Home based - Americas, San Francisco',
+        'Home based - Americas, Seattle',
+        # Canada
+        'Home based - Americas, Calgary',
+        'Home based - Americas, Montreal',
+        'Home based - Americas, Ottawa',
+        'Home based - Americas, Toronto',
+        'Home based - Americas, Vancouver',
+        'Home based - Americas, Victoria',
+        # South America
+        'Home based - Americas, Buenos Aires',
+        'Home based - Americas, Mexico City',
+        'Home based - Americas, Santiago',
+        'Home based - Americas, São Paulo',
+    ],
+    'emea': [
+        'Home based - Europe, Amsterdam',
+        'Home based - Europe, Barcelona',
+        'Home based - Europe, Berlin',
+        'Home based - Europe, Copenhagen',
+        'Home based - Europe, Dublin',
+        'Home based - Europe, Eindhoven',
+        'Home based - Europe, Hamburg',
+        'Home based - Europe, Helsinki',
+        'Home based - Europe, London',
+        'Home based - Europe, Munich',
+        'Home based - Europe, Rotterdam',
+        'Home based - Europe, Stockholm',
+        'Home based - Europe, Stuttgart',
+    ],
+    'apac': [
+    ]
+}
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Duplicate Greenhouse job postings to multiple locations.')
+    parser.add_argument(
+        'job_ids', nargs='+',
+        help='The numeric Greenhouse job id (the number in the URL when on the Job Dashboard)')
+    parser.add_argument(
+        '--region', dest='regions', nargs='+', choices=['americas', 'emea', 'apac'],
+        help='The regions in which to create job postings')
+    parser.add_argument(
+        '--browser', dest='browser', choices=['chrome', 'firefox'], default='chrome',
+        help='The browser to use (default is chrome)')
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    if args.browser == 'firefox':
+        browser = webdriver.Firefox()
+    else:
+        browser = webdriver.Chrome()
+
+    new_browser = True
+
+    for job_id in args.job_ids:
+        job_posts_page_url = f'{SITE}/plans/{job_id}/jobapp'
+        browser.get(job_posts_page_url)
+
+        if new_browser:
+            # click Accept Cookies button
+            accept_cookies_btn = browser.find_elements_by_xpath('//*[@id="cookie-policy-button-accept"]')
+            if accept_cookies_btn:
+                accept_cookies_btn[0].click()
+
+            # enter Ubuntu SSO email and password
+            email_txt = browser.find_element_by_id("id_email")
+            if email_txt:
+                email_txt.send_keys(SSO_EMAIL)
+
+            password_txt = browser.find_element_by_id("id_password")
+            if password_txt:
+                password_txt.send_keys(SSO_PASSWORD)
+
+            continue_btn = browser.find_elements_by_xpath('//button[@name="continue"]')
+            if continue_btn:
+                continue_btn[0].click()
+            new_browser = False
+
+        # pause 25 seconds for 2-factor auth by user
+        wait = ui.WebDriverWait(browser, 25) # timeout after 25 seconds
+
+        multipage = False
+        existing_locations = []
+        while True:
+            results = wait.until(lambda browser: browser.find_elements_by_class_name('job-application__offices'))
+            # minimize trays so they don't obstruct clicks
+            trays = browser.find_elements_by_xpath('//div[@data-provides="tray-close"]')
+            for tray in trays:
+                tray.click()
+
+            existing_locations += [result.text.strip('()') for result in results]
+            next_page =  browser.find_elements_by_css_selector('a.next_page')
+            if next_page:
+                multipage = True
+                next_page[0].click()
+            else:
+                break
+
+        # return to first page of job posts
+        if multipage:
+            browser.get(job_posts_page_url)
+
+        for region in args.regions:
+            region_locations = REGIONS[region]
+            new_locations = set(region_locations) - set(existing_locations)
+            for location_text in sorted(new_locations):
+                publish_location_text = location_text.split(',')[-1].strip()
+
+                duplicate_link = wait.until(lambda browser: browser.find_elements_by_xpath('//*[@id="job_applications"]//tr[1]//a[text()="Duplicate"]'))
+                page = duplicate_link[0].get_attribute('href')
+                browser.get(page)
+
+                job_name_txt = browser.find_elements_by_xpath('//input[../label="Job Name"]')[0]
+                job_name = job_name_txt.get_attribute('value').replace('Copy of ', '').strip()
+                job_name_txt.clear()
+                job_name_txt.send_keys(job_name)
+
+                post_to = browser.find_elements_by_xpath('//label[text()="Post To"]/..//input[1]')[0]
+                post_to.send_keys(JOB_BOARD)
+                post_to.send_keys(Keys.ENTER)
+
+                location = browser.find_elements_by_xpath('//label[text()="Location"]/..//input[1]')[0]
+                location.send_keys(location_text)
+
+                browser.find_elements_by_xpath('//label[text()="Glassdoor"]/input[1]')[0].click()
+                browser.find_elements_by_xpath('//label[text()="Indeed"]/input[1]')[0].click()
+                browser.find_elements_by_xpath('//label[text()="Remote"]/input[1]')[0].click()
+                publish_location = browser.find_elements_by_xpath('//input[@placeholder="Select location"]')[0]
+                publish_location.send_keys(publish_location_text)
+                popup_menu_xpath = (
+                    f'//ul[contains(@class, "ui-menu")]'
+                    f'/li[contains(@class, "ui-menu-item")]'
+                    f'/div[contains(text(), "{publish_location_text}")]'
+                )
+                location_choices = wait.until(lambda browser: browser.find_elements_by_xpath(popup_menu_xpath))
+                publish_location.send_keys(Keys.DOWN)
+                publish_location.send_keys(Keys.TAB)
+                time.sleep(.5)
+
+                # click the Save button
+                save_btn = browser.find_elements_by_xpath('//a[text()="Save"]')[0]
+                save_btn.click()
+
+                #publish_btns = wait.until(lambda browser: browser.find_elements_by_css_selector('tr.job-application.draft img.publish-application-button'))
+                wait.until(lambda browser: browser.find_elements_by_class_name('job-application__offices'))
+                publish_btns = browser.find_elements_by_css_selector('tr.job-application.draft img.publish-application-button')
+                for btn in publish_btns:
+                    btn.click()
+                    time.sleep(.2)
+
+        while True:
+            results = wait.until(lambda browser: browser.find_elements_by_class_name('job-application__offices'))
+            publish_btns = browser.find_elements_by_css_selector('tr.job-application.draft img.publish-application-button')
+            for btn in publish_btns:
+                btn.click()
+                time.sleep(.2)
+            next_page =  browser.find_elements_by_css_selector('a.next_page')
+            if next_page:
+                next_page[0].click()
+            else:
+                break
+
+if __name__ == '__main__':
+    main()
